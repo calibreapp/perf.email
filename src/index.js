d07ff622d0249f359774ec422d01fbc62c831898
Ben Schwarz <ben@germanforblack.com>
2018-02-12 11:27:05 +1100

    Don’t use JS signup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-'use strict'
+"use strict";
 
 // CSS
-import tachyons from 'tachyons/css/tachyons.css'
-import styles from './style.css'
-import logo from './logo.svg'
+import tachyons from "tachyons/css/tachyons.css";
+import styles from "./style.css";
+import logo from "./logo.svg";
 
-import formHandler from './form-submit.js'
-import graphAnimationHandler from './graph-animation.js'
+import graphAnimationHandler from "./graph-animation.js";
