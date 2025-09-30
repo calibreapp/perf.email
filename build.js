20b702475492a9ccff8e69681b8f4d521525868c
Ben Schwarz <ben@calibreapp.com>
2025-09-30 15:11:33 +1000

    Update build.js

--- build.js
+++ build.js
@@ -5,25 +5,6 @@ mkdirSync("dist", { recursive: true });
 cpSync("index.html", "dist/index.html");
 cpSync("style.css", "dist/style.css");
 cpSync("node_modules/normalize.css/normalize.css", "dist/normalize.css");
-
-const staticDirs = [
-  "favicons",
-  "social",
-  "testimonials",
-  "perf-email-preview-dark.webp",
-  "perf-email-preview-light.webp",
-  "perf-email-preview-light.png",
-  "calibre-logo.svg",
-  "aboriginal-flag.svg",
-  "manifest.webmanifest"
-];
-
-staticDirs.forEach((item) => {
-  try {
-    cpSync(item, `dist/${item}`, { recursive: true });
-  } catch (err) {
-    console.log(`Skipping ${item} (not found)`);
-  }
-});
+cpSync("public", "dist", { recursive: true });
 
 console.log("Static files copied to dist/");
